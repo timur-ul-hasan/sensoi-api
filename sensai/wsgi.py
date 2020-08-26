47b8596009d872a261384032990b61097307f245
Hasan Javed <hasanjaved065@gmail.com>
2020-08-26 12:48:51 +0500

    Restructure Application for deployment.

--- sensai/wsgi.py
+++ sensai/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sensoi.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sensai.settings')
 
 application = get_wsgi_application()
