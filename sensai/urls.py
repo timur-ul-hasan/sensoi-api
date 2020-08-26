b57e1d28dd6b1bf3932833b233f6c34aaa6ebabd
Hasan Javed <hasanjaved065@gmail.com>
2020-08-26 13:00:43 +0500

    changes in url

--- sensai/urls.py
+++ sensai/urls.py
@@ -22,7 +22,7 @@ schema_view = get_schema_view(
 
 urlpatterns = [
     path(r'^docs(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
-    path('docs/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('api/',include('user.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
